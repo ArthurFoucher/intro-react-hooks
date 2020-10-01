f67e98092f0bba6d1011a667343886e6a78b2338
Arthur <foucherarthur@gmail.com>
2020-10-01 12:50:57 +0200

    Use useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 export const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className='container'>
-      Ready to get started
+      <div>count: {count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
+      </div>
     </div>
   );
 };
